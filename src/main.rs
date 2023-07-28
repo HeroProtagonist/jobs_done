e3e026c724fa2e8a26603fa4c9595f02618accf4
HeroProtagonist <jstepper91@gmail.com>
2023-07-28 17:48:50 -0400

    Add option to just play sound

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ pub enum Sound {
 #[derive(Debug, Parser)]
 #[command(version, arg_required_else_help(true))]
 struct Cli {
+    /// Just play the sound without supplying a command
+    #[clap(long, short)]
+    play: bool,
     /// Sound to play
     #[arg(short, long)]
     #[clap(value_enum, default_value_t=Sound::JobsDone)]
@@ -26,6 +29,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+
+    if args.play {
+        play_sound(args.sound).unwrap();
+        exit(0);
+    }
 
     let mut command = Command::new(&args.command[0]);
 
